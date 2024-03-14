3802a7aa4e0fb2822e32373f9bf48499c9623428
Alier <alierditmaguet@gmail.com>
2024-03-14 02:46:35 +0200

    authentication setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  mount_devise_token_auth_for 'User', at: '/api/v1/auth'
+  namespace :api do
+    namespace :v1 do
+      resources :books
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
