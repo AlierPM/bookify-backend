17b5c2f2bd81324d440be48c3e41aae4c46b1b4b
Alier <alierditmaguet@gmail.com>
2024-03-14 15:49:49 +0200

    add favorites books endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: '/api/v1/auth'
   namespace :api do
     namespace :v1 do
+      resources :favorites
       resources :books
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_13_202121) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_14_093004) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_13_202121) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_favorites_on_book_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,4 +59,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_13_202121) do
   end
 
   add_foreign_key "books", "users"
+  add_foreign_key "favorites", "books"
+  add_foreign_key "favorites", "users"
 end
