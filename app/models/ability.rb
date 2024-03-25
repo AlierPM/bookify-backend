3187e64402f17e542e6dbe72abdc5463e837d00c
ometman <revomet@gmail.com>
2024-03-25 17:35:30 +0000

    Modify cancan ability for users and admins

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,29 +4,19 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the user here. For example:
-    #
-    #   return unless user.present?
-    #   can :read, :all
-    #   return unless user.admin?
-    #   can :manage, :all
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, published: true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
+    # No guest access
+    return unless user.present?
+
+    # Define abilities based on user roles or permissions
+    if user.admin?
+      can :manage, :all
+    else
+      can [:read, :create, :update, :destroy], Favorite
+      can [:read, :create, :update, :destroy], Rental_Request
+      can :read, Book
+      cannot [:create, :update, :destroy], Book
+      cannot [:read, :create, :update, :destroy], User
+    end
   end
 end
+
