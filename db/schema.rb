66997ecc6e8dbaa668111c30d9f8e71f9b74ac8a
ometman <revomet@gmail.com>
2024-03-26 01:02:04 +0000

    Modify user model to add user and admin roles and modify migration to add 'user' as default

--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_26_004520) do
     t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "role"
+    t.string "role", default: "user"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
