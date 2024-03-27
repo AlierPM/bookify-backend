5d80603c6c608e2f393bbc59e8e263045b89cc83
ometman <revomet@gmail.com>
2024-03-27 13:01:52 +0000

    Add authorization to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  include CanCan::ControllerAdditions
+
+  # Load and authorize abilities
+  before_action :load_and_authorize_resource
 
   respond_to :json
+
+  rescue_from CanCan::AccessDenied do |exception|
+    render json: { error: 'Access denied' }, status: :forbidden
+  end
+
+  private
+
+  def load_and_authorize_resource
+    @current_ability ||= Ability.new(current_user)
+    authorize! action_name.to_sym, controller_name.to_sym
+  end
 end
