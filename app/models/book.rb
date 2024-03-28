2c03050975f818f6efe4e9c5878da7820dff1dd6
ometman <revomet@gmail.com>
2024-03-28 08:24:38 +0000

    Revert "Backend: Capstone Project External Support Review Request "

--- app/models/book.rb
+++
@@ -1,5 +0,0 @@
-class Book < ApplicationRecord
-  belongs_to :user
-  has_many :rental_requests
-  has_many :favorites
-end
--- test/models/rental_request_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RentalRequestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
